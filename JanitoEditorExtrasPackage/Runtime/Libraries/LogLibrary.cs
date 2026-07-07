9b6265b51c2abbfd2942c29672fe34beca7410b3
Jan <janfrancoherrera@gmail.com>
2026-07-07 21:39:20 +0100

    Refactor to use conditional attributes
    Replace conditional compilation with conditional attributes on the log library methods to improve readability.
    
    Added equivalent static logging functions that allow for any type with optional context, however non unity objects must use the library directly instead of extension methods.

--- JanitoEditorExtrasPackage/Runtime/Libraries/LogLibrary.cs
+++ JanitoEditorExtrasPackage/Runtime/Libraries/LogLibrary.cs
@@ -1,7 +1,20 @@
+using System.Diagnostics;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 namespace Janito.EditorExtras
 {
+    /// <summary>
+    /// Collection of logging extension methods and functions to log messages with a prefix of the type of the object for easy identification. It includes editor and development build only versions of the methods to avoid unnecessary logs in production builds, and non <c>UnityEngine.Object</c> contexts versions that require the type to be specified as a generic parameter.
+    /// </summary>
+    /// <remarks>
+    /// Non <c>UnityEngine.Object</c> contexts do not include an extension method the same way <c>UnityEngine.Object</c> classes do to avoid ambiguity when accessing extension method. For non <c>UnityEngine.Object</c> contexts, use the generic version of the method and specify the type as the generic parameter using <c>LogLibrary</c>.
+    /// </remarks>
+    /// <example>
+    /// LogLibrary.LogInDevelopment<SomeClass>("This is a log message from SomeClass."); // Without context
+    /// LogLibrary.LogInDevelopment<SomeClass>("This is a log message from SomeClass with context.", someObject); // With context
+    /// this.LogInDevelopment("This is a log message from this object."); // Logging with extension method in any <c>UnityEngine.Object</c> class.
+    /// </example>
     public static class LogLibrary
     {
         #region Editor/Development Only
@@ -10,33 +23,54 @@ namespace Janito.EditorExtras
         /// </summary>
         /// <param name="object"></param>
         /// <param name="message"></param>
+        [Conditional("UNITY_EDITOR"), Conditional("DEVELOPMENT_BUILD")]
         public static void LogInDevelopment(this Object @object, string message)
         {
-#if UNITY_EDITOR || DEVELOPMENT_BUILD
             @object.LogPrefixed(message);
-#endif
         }
 
+        [Conditional("UNITY_EDITOR"), Conditional("DEVELOPMENT_BUILD")]
         public static void LogWarningInDevelopment(this Object @object, string message)
         {
-#if UNITY_EDITOR || DEVELOPMENT_BUILD
             @object.LogWarningPrefixed(message);
-#endif
         }
 
+        [Conditional("UNITY_EDITOR"), Conditional("DEVELOPMENT_BUILD")]
         public static void LogErrorInDevelopment(this Object @object, string message)
         {
-#if UNITY_EDITOR || DEVELOPMENT_BUILD
-            @object.LogErrorPrefixed(message);  
-#endif
+            @object.LogErrorPrefixed(message);
         }
 
+        [Conditional("UNITY_EDITOR"), Conditional("DEVELOPMENT_BUILD")]
         public static void LogAssertionInDevelopment(this Object @object, string message)
         {
-#if UNITY_EDITOR || DEVELOPMENT_BUILD
             @object.LogAssertionPrefixed(message);
-#endif
         }
+
+        [Conditional("UNITY_EDITOR"), Conditional("DEVELOPMENT_BUILD")]
+        public static void LogInDevelopment<T>(string message, Object context = null)
+        {
+            LogPrefixed<T>(message, context);
+        }
+
+        [Conditional("UNITY_EDITOR"), Conditional("DEVELOPMENT_BUILD")]
+        public static void LogWarningInDevelopment<T>(string message, Object context = null)
+        {
+            LogWarningPrefixed<T>(message, context);
+        }
+
+        [Conditional("UNITY_EDITOR"), Conditional("DEVELOPMENT_BUILD")]
+        public static void LogErrorInDevelopment<T>(string message, Object context = null)
+        {
+            LogErrorPrefixed<T>(message, context);
+        }
+
+        [Conditional("UNITY_EDITOR"), Conditional("DEVELOPMENT_BUILD")]
+        public static void LogAssertionInDevelopment<T>(string message, Object context = null)
+        {
+            LogAssertionPrefixed<T>(message, context);
+        }
+
         #endregion
 
         /// <summary>
@@ -44,7 +78,7 @@ namespace Janito.EditorExtras
         /// </summary>
         /// <param name="object"></param>
         /// <param name="message"></param>
-        public static void LogPrefixed(this Object @object, string message) 
+        public static void LogPrefixed(this Object @object, string message)
         {
             Debug.Log($"[{@object.GetType().Name}] " + message, @object);
         }
@@ -62,6 +96,26 @@ namespace Janito.EditorExtras
         public static void LogAssertionPrefixed(this Object @object, string message)
         {
             Debug.LogAssertion($"[{@object.GetType().Name}] " + message, @object);
+        }
+
+        public static void LogPrefixed<T>(string message, Object context = null)
+        {
+            Debug.Log($"[{typeof(T).Name}] " + message, context);
+        }
+
+        public static void LogWarningPrefixed<T>(string message, Object context = null)
+        {
+            Debug.LogWarning($"[{typeof(T).Name}] " + message, context);
+        }
+
+        public static void LogErrorPrefixed<T>(string message, Object context = null)
+        {
+            Debug.LogError($"[{typeof(T).Name}] " + message, context);
+        }
+
+        public static void LogAssertionPrefixed<T>(string message, Object context = null)
+        {
+            Debug.LogAssertion($"[{typeof(T).Name}] " + message, context);
         }
     }
 }
