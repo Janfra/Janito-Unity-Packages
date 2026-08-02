58ecea8c9c67373e90eaefdab1e32e427e5b4be5
Jan <janfrancoherrera@gmail.com>
2026-08-02 21:07:55 +0100

    docs(editor): Add TypeCriteria extension comments
    Add extension helpers comments to `TypeCriteriaExtensions`: `RequireInterface`, `RequireAbstract`, `RequireGeneric`, `ExcludeInterface`, `ExcludeAbstract`, and `ExcludeGeneric`. Each method clears conflicting `TypeRequirementFlags` (removing the opposite `Not*` flag) and returns a new `TypeCriteria` with the requested requirement.

--- JanitoEditorExtrasPackage/Editor/Extensions/TypeCriteriaExtensions.cs
+++ JanitoEditorExtrasPackage/Editor/Extensions/TypeCriteriaExtensions.cs
@@ -2,35 +2,66 @@ namespace Janito.EditorExtras.Editor
 {
     public static class TypeCriteriaExtensions
     {
+        /// <summary>
+        /// Adds a requirement to the TypeCriteria that the type must be an interface. If the TypeCriteria already has a requirement that the type must not be an interface, this will override that requirement.
+        /// </summary>
+        /// <param name="typeCriteria">The TypeCriteria to modify</param>
+        /// <returns>A new TypeCriteria with the interface requirement added</returns>
         public static TypeCriteria RequireInterface(this TypeCriteria typeCriteria)
         {
             var filteredExistingFlags = typeCriteria.TypeRequirements & ~TypeCriteria.TypeRequirementFlags.NotInterface;
             return new TypeCriteria(filteredExistingFlags | TypeCriteria.TypeRequirementFlags.Interface);
         }
 
+        /// <summary>
+        /// Adds a requirement to the TypeCriteria that the type must be abstract. If the TypeCriteria already has a requirement that the type must not be abstract, this will override that requirement.
+        /// </summary>
+        /// <param name="typeCriteria">The TypeCriteria to modify</param>
+        /// <returns>A new TypeCriteria with the abstract requirement added</returns>
         public static TypeCriteria RequireAbstract(this TypeCriteria typeCriteria)
         {
             var filteredExistingFlags = typeCriteria.TypeRequirements & ~TypeCriteria.TypeRequirementFlags.NotAbstract;
             return new TypeCriteria(filteredExistingFlags | TypeCriteria.TypeRequirementFlags.Abstract);
         }
 
+        /// <summary>
+        /// Adds a requirement to the TypeCriteria that the type must be generic. If the TypeCriteria already has a requirement that the type must not be generic, this will override that requirement. 
+        /// </summary>
+        /// <param name="typeCriteria">The TypeCriteria to modify</param>
+        /// <returns>A new TypeCriteria with the generic requirement added</returns>
         public static TypeCriteria RequireGeneric(this TypeCriteria typeCriteria)
         {
             var filteredExistingFlags = typeCriteria.TypeRequirements & ~TypeCriteria.TypeRequirementFlags.NotGeneric;
             return new TypeCriteria(filteredExistingFlags | TypeCriteria.TypeRequirementFlags.Generic);
         }
+
+        /// <summary>
+        /// Adds a requirement to the TypeCriteria that the type must not be an interface. If the TypeCriteria already has a requirement that the type must be an interface, this will override that requirement.
+        /// </summary>
+        /// <param name="typeCriteria">The TypeCriteria to modify</param>
+        /// <returns>A new TypeCriteria with the not interface requirement added</returns>
         public static TypeCriteria ExcludeInterface(this TypeCriteria typeCriteria)
         {
             var filteredExistingFlags = typeCriteria.TypeRequirements & ~TypeCriteria.TypeRequirementFlags.Interface;
             return new TypeCriteria(filteredExistingFlags | TypeCriteria.TypeRequirementFlags.NotInterface);
         }
 
+        /// <summary>
+        /// Adds a requirement to the TypeCriteria that the type must not be abstract. If the TypeCriteria already has a requirement that the type must be abstract, this will override that requirement.
+        /// </summary>
+        /// <param name="typeCriteria">The TypeCriteria to modify</param>
+        /// <returns>A new TypeCriteria with the not abstract requirement added</returns>
         public static TypeCriteria ExcludeAbstract(this TypeCriteria typeCriteria)
         {
             var filteredExistingFlags = typeCriteria.TypeRequirements & ~TypeCriteria.TypeRequirementFlags.Abstract;
             return new TypeCriteria(filteredExistingFlags | TypeCriteria.TypeRequirementFlags.NotAbstract);
         }
 
+        /// <summary>
+        /// Adds a requirement to the TypeCriteria that the type must not be generic. If the TypeCriteria already has a requirement that the type must be generic, this will override that requirement.
+        /// </summary>
+        /// <param name="typeCriteria">The TypeCriteria to modify</param>
+        /// <returns>A new TypeCriteria with the not generic requirement added</returns>
         public static TypeCriteria ExcludeGeneric(this TypeCriteria typeCriteria)
         {
             var filteredExistingFlags = typeCriteria.TypeRequirements & ~TypeCriteria.TypeRequirementFlags.Generic;
