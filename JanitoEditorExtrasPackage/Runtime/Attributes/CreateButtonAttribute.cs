3767792daeee4f6e29ab7c10f5ae34c8fc5439e5
Jan <93995100+Janfra@users.noreply.github.com>
2026-08-20 21:43:06 +0100

    feat(editor): add create button attribute (#2)

---
+++ JanitoEditorExtrasPackage/Runtime/Attributes/CreateButtonAttribute.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Diagnostics;
+using UnityEngine;
+
+namespace Janito.EditorExtras
+{
+    [Conditional("UNITY_EDITOR")]
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+    public class CreateButtonAttribute : PropertyAttribute 
+    {
+        public const string k_DefaultSavePath = "Assets"; // Requires 'Assets' to be the root for valid save paths
+        public readonly string NamingFormat;
+        public readonly string SavePath;
+
+        public CreateButtonAttribute(string namingFormat = null, string savePath = k_DefaultSavePath)
+        {
+            savePath.Trim();
+            if (string.IsNullOrEmpty(savePath) || string.IsNullOrWhiteSpace(savePath))
+            {
+                savePath = k_DefaultSavePath;
+            }
+            else if(!savePath.StartsWith(k_DefaultSavePath))
+            {
+                LogLibrary.LogWarningInDevelopment<CreateButtonAttribute>($"Create Button save path must start with '{k_DefaultSavePath}' as the root. Using default path instead. Provided save path: {savePath}");
+                savePath = k_DefaultSavePath;
+            }
+
+            NamingFormat = namingFormat;
+            SavePath = savePath;
+        }
+
+        public CreateButtonAttribute()
+        {
+            SavePath = k_DefaultSavePath;
+            NamingFormat = null;
+        }
+    }
+}
